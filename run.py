0d656759092a33a589d5d5b1fb8a02ab234f5042
Tal Linzen <tal.linzen@gmail.com>
2010-03-27 18:54:41 +0300

    x

--- run.py
+++ run.py
@@ -11,9 +11,7 @@ from filters.subcat import SubcategorizationFrames
 from filters.pd_annotation import PDAnnotation
 from filters.subcat_annotation import SubcatAnnotation
 from bgutag import BGUFile, BGUDir, BGUQuery
-
-interesting_verbs = [u'חפץ', u'התנגד', u'התנדב', u'הכחיש', u'כרסם', u'חייג', u'הצהיר', u'התבשר', u'התחרט',
-        u'נקט', u'התפתה', u'מיעט', u'התגעגע', u'התקרב']
+from verbs_for_subcat import verbs_for_subcat
 
 cleaner = IsrablogCleaner()
 harvester = IsrablogHarvester()
@@ -24,13 +22,6 @@ def pheb(s):
     for line in h.split('\n'):
         p.append(''.join(reversed(line)))
     print '\n'.join(p)
-
-def dump(s):
-    codecs.open('/tmp/t.html', 'w', 'utf16').write(s.clean_text.decode('utf8').replace('\n', '<br>'))
-    codecs.open('/tmp/r.html', 'w', 'utf16').write(s.raw.decode('cp1255'))
-
-def pheb2(s):
-    print deu(s.decode('utf-8')).decode('cp1255')
 
 def age_histogram():
     return WebPage._connection.queryAll('select age, count(*) from user group by age')
