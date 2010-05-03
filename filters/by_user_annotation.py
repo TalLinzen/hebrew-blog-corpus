45d7dcdea9273420591976bd0d31ffb45fa256ab
Tal Linzen <tlinzen@il.ibm.com>
2010-05-03 11:20:38 +0300

    More work on generic filter

--- filters/by_user_annotation.py
+++ filters/by_user_annotation.py
@@ -1,22 +1,27 @@
 from annotation import Annotation
 
-def group_by_attribute(attribute, sentences):
-    by_attribute = {}
+def group_by_attributes(attributes, sentences):
+    if isinstance(attributes, basestring):
+        attributes = [attributes]
+    by_attributes = {}
     for sentence in sentences:
-        value = sentence.metadata.get(attribute, 'None')
-        if value == None:
-            value = 'None'
-        by_attribute.setdefault(value, []).append(sentence)
-    return by_attribute
+        values = []
+        for attribute in attributes:
+            value = sentence.metadata.get(attribute, 'None')
+            if value == None:
+                value = 'None'
+            values.append(value)
+        by_attributes.setdefault('_'.join(values), []).append(sentence)
+    return by_attributes
 
-def mix_attribute(attribute, sentences, limit):
+def mix_attributes(attributes, sentences, limit):
     result = []
     index = 0
     limit = min(limit, len(sentences))
-    by_attribute = group_by_attribute(attribute, sentences)
+    by_attributes = group_by_attributes(attributes, sentences)
 
     while len(result) < limit:
-        for sentence_group in by_attribute.values():
+        for sentence_group in by_attributes.values():
             if len(result) == limit:
                 break
             elif index < len(sentence_group):
@@ -27,7 +32,7 @@ def mix_attribute(attribute, sentences, limit):
 
 class ByAttributeAnnotation(Annotation):
 
-    def __init__(self, description, attribute, single_workbook=False,
+    def __init__(self, description, attributes, single_workbook=False,
             single_workbook_name='All', min_tokens=2, max_tokens=3000):
         '''
         single_workbook: if True, all attribute values will be on the same
@@ -40,21 +45,21 @@ class ByAttributeAnnotation(Annotation):
         '''
 
         Annotation.__init__(self, description)
-        self.attribute = attribute
+        self.attributes = attributes
         self.single_workbook = single_workbook
         self.single_workbook_name = single_workbook_name
         self.min_tokens = min_tokens
         self.max_tokens = max_tokens
 
     def create_workbooks(self):
-        by_attribute = group_by_attribute(self.attribute, self.sentences)
+        by_attributes = group_by_attributes(self.attributes, self.sentences)
         d = {}
-        for value, sentences in by_attribute.items():
+        for value, sentences in by_attributes.items():
             if len(sentences) >= self.min_tokens:
-                d[value] = mix_attribute('user', sentences, self.max_tokens)
+                d[value] = mix_attributes('user', sentences, self.max_tokens)
 
         if self.single_workbook:
-            self.workbooks = {self.single_workbook_name: by_attribute}
+            self.workbooks = {self.single_workbook_name: by_attributes}
         else:
             self.workbooks = dict((value, {value: sentences}) for \
                     value, sentences in d.items())
@@ -65,5 +70,5 @@ class MixUsers(Annotation):
         self.limit = limit
 
     def create_workbooks(self):
-        mixed = mix_attribute('user', self.sentences, self.limit)
+        mixed = mix_attributes('user', self.sentences, self.limit)
         self.workbooks = {'mixed': {'Mixed': mixed}}
--- filters/generic.py
+++ filters/generic.py
@@ -1,10 +1,69 @@
 # -*- coding: utf-8 -*-
 from filter import Filter
 from .tools.hspell import infinitives
+import unittest
+
+clitic_forms = [u'י', u'ך', u'ו', u'ה', u'נו', u'כם', u'כן', u'ם', u'ן']
+clitic_forms_special = [u'י', u'ך', u'ו', u'ה', u'נו', u'כם', u'כן', u'הם', u'הן']
+
+def is_obstructor(word, state):
+    return word.pos == 'punctuation' or word.prefix in (u'ש', u'וש')
+
+def Equal(field, value):
+    def predicate(word, state):
+        return getattr(word, field) == value
+    return predicate
+
+def OneOf(field, values, export_field=None):
+    if export_field:
+        def predicate(word, state):
+            attr = getattr(word, field)
+            if attr in values:
+                state[export_field] = attr
+                return True
+    else:
+        def predicate(word, state):
+            return getattr(word, field) in values
+    return predicate
+
+class PredicateModifier(object):
+    def __init__(self, predicate):
+        self.predicate = predicate
+
+class Once(PredicateModifier):
+    def parse(self, index, sentence, state):
+        matched = self.predicate(sentence.rich_words[index], state)
+        if matched:
+            index += 1
+        return matched, index
+
+class Repeated(PredicateModifier):
+    unlimited = -1
+    def __init__(self, predicate, at_least=0, at_most=unlimited):
+        PredicateModifier.__init__(self, predicate)
+        self.at_least = at_least
+        self.at_most = at_most
+    def parse(self, index, sentence, state):
+        original_index = index
+        while self.predicate(sentence.rich_words[index], state) and \
+                (index - original_index <= self.at_most or \
+                self.at_most == Repeated.unlimited):
+            index += 1
+
+        matched = index - original_index >= self.at_least
+        return matched, index
+
+class AnyNumberOf(Repeated):
+    '''
+    An alias for Repeated(0, unlimited), for clarity
+    '''
+    def __init__(self, predicate):
+        Repeated.__init__(self, predicate, 0, Repeated.unlimited)
 
 class GenericFilter(Filter):
 
     predicates = []
+    private_variables = []
 
     def __init__(self):
         Filter.__init__(self)
@@ -14,34 +73,42 @@ class GenericFilter(Filter):
         self.predicate_options = []
         for predicate in self.predicates:
             if type(predicate) != tuple:
-                self.processed_predicates.append((predicate, {}))
+                self.processed_predicates.append((self.wrap_in_modifier(predicate), {}))
             else:
-                self.processed_predicates.append(predicate)
+                self.processed_predicates.append((self.wrap_in_modifier(predicate[0]), predicate[1]))
 
-    def is_obstructor(self, word):
-        return word.pos == 'punctuation' or word.prefix in (u'ש', u'ו', u'וש')
+    def wrap_in_modifier(self, predicate):
+        if not isinstance(predicate, PredicateModifier):
+            return Once(predicate)
+        else:
+            return predicate
 
     def process(self, sentence):
-        metadatas = []
-        for index, word in enumerate(sentence.rich_words):
-            predicate, options = self.processed_predicates[len(metadatas)]
-            new_metadata = predicate(word)
-            if new_metadata == True:
-                new_metadata = {}
-            if type(new_metadata) == dict:
-                if options.get('highlight', False):
-                    # Currently supports only single word highlight
-                    sentence.highlight = (index, index)
-                metadatas.append(new_metadata)
-            else:
-                metadatas = []
+        state = {}
+        index = 0
+        next_predicate = 0
+        while index != len(sentence.rich_words):
+            predicate, options = self.processed_predicates[next_predicate]
+            old_index = index
+            matched, index = predicate.parse(index, sentence, state)
 
-            if len(metadatas) == len(self.predicates):
+            if matched:
+                next_predicate += 1
+                if options.get('highlight', False):
+                    sentence.highlight = (old_index, index - 1)
+            else:
+                assert index == old_index, "Shouldn't change index if didn't match"
+                index += 1
+                next_predicate = 0
+                state = {}
+
+            if next_predicate == len(self.predicates):
                 break
 
-        if len(metadatas) == len(self.predicates):
-            for metadata in metadatas:
-                sentence.metadata.update(metadata)
+        if next_predicate == len(self.predicates):
+            for key, value in state.items():
+                if value not in self.private_variables:
+                    sentence.metadata[key] = state[key]
             self.post_process(sentence)
             return True
         else:
@@ -52,16 +119,15 @@ class GenericFilter(Filter):
 
 class Bishvil(GenericFilter):
 
-    clitic_forms = [u'י', u'ך', u'ו', u'ה', u'נו', u'כם', u'כן', u'ם', u'ן']
     bishvil_fused = [u'בשביל' + c for c in clitic_forms]
 
-    def bishvil(word):
+    def bishvil(word, state):
         if word.word == u'בשביל':
-            return {'argument': 'lexical'}
+            state['argument'] = 'lexical'; return True
         elif word.word in Bishvil.bishvil_fused:
-            return {'argument': 'pronoun'}
+            state['argument'] = 'pronoun'; return True
 
-    def not_conjunction(word):
+    def not_conjunction(word, state):
         good = word.tense != 'inf' and \
                 (word.prefix is None or u'ש' not in word.prefix) and \
                 word.word not in infinitives
@@ -72,27 +138,103 @@ class Bishvil(GenericFilter):
         not_conjunction
     ]
 
-from dative import is_dative, lamed_fused_forms
+from dative import is_dative
+from .tools.hspell import infinitives
+
+lamed_fused_forms = [u'ל' + form for form in clitic_forms_special]
+lamed_reflexive_fused_forms = [u'לעצמ' + form for form in clitic_forms]
+lamed_quasi_pronouns = [u'לאנשים', u'לדברים', u'לזה']
+
+def is_dative_wrapped(word, state):
+    if is_dative(word):
+        if word.word in lamed_fused_forms or word.word in lamed_reflexive_fused_forms:
+            state['argument'] = 'pronoun'
+        elif word.word in lamed_quasi_pronouns:
+            state['argument'] = 'quasi'
+        else:
+            state['argument'] = 'lexical'
+        return True
 
 class YeshDative(GenericFilter):
-
-    def yesh_or_ein(word):
+    # Annotate by 'lemma' and 'argument'
+    def yesh_or_ein(word, state):
         if word.word in (u'יש', u'אין'):
-            return {'lemma': word.word}
-
-    def is_dative_wrapped(word):
-        if is_dative(word):
-            if word.word in lamed_fused_forms:
-                return {'argument': 'pronoun'}
-            else:
-                return {'argument': 'lexical'}
-
-    def post_process(self, sentence):
-        sentence.metadata['lemma_and_argument'] = '%s_%s' % \
-                (sentence.metadata['lemma'], sentence.metadata['argument'])
+            state['lemma'] = word.word
+            return True
 
     predicates = [
-        yesh_or_ein,
+        OneOf('word', (u'יש', u'אין'), export_field='lemma'),
         (is_dative_wrapped, {'highlight': True})
     ]
 
+class NatanDative(GenericFilter):
+    # Annotate by 'argument'
+    def noun(word, state):
+        return word.pos == 'noun' and word.word not in infinitives
+
+    def not_infinitive(word, state):
+        return word.word not in infinitives
+
+    predicates = [
+            Equal('lemma', u'נתן'),
+            (is_dative_wrapped, {'highlight': True}),
+            AnyNumberOf(Equal('chunk', 'I-NP')),
+            OneOf('chunk', ['B-NP', 'I-NP']),
+            AnyNumberOf(Equal('chunk', 'I-NP')),
+            not_infinitive
+        ]
+
+class PossessiveDative(GenericFilter):
+    verbs = {
+        u'אכל': u'את',
+        u'הרס': u'את',
+        u'הרים': u'את',
+        u'הסתכל': u'על'
+    }
+
+    def is_the_expected_preposition(word, state):
+        if word.prefix in (u'ש', u'וש'):
+            return False
+
+        expected_prepositions = PossessiveDative.verbs[state['verb']]
+        if isinstance(expected_prepositions, basestring):
+            expected_prepositions = (expected_prepositions,)
+        return len(set((word.lemma, word.prefix)) & set(expected_prepositions)) > 0
+
+    predicates = [
+            OneOf('lemma', set(verbs.keys()), export_field='verb'),
+            (is_dative_wrapped, {'highlight': True}),
+            AnyNumberOf(Equal('chunk', 'I-NP')),
+            is_the_expected_preposition
+        ]
+
+class DS(object):
+    'Dummy Sentence'
+    def __init__(self, words):
+        self.rich_words = words
+        self.metadata = {}
+
+class Tests(unittest.TestCase):
+
+    def test_repeated(self):
+        sentence = DS(list('abbbccd'))
+        class TestFilter(GenericFilter):
+            predicates = [lambda x: x == 'a',
+                    Repeated(lambda x: x == 'b', 3, 3),
+                    lambda x: x == 'c']
+        tf = TestFilter()
+        self.assertTrue(tf.process(sentence))
+
+    def test_anynumber(self):
+        sentence = DS(list('abbbccd'))
+        class TestFilter(GenericFilter):
+            predicates = [
+                    AnyNumberOf(lambda x: x == 'o'),
+                    Repeated(lambda x: x == 'b', 1, 4),
+                    lambda x: x == 'c']
+        tf = TestFilter()
+        self.assertTrue(tf.process(sentence))
+
+    def runTest(self):
+        self.test_repeated()
+        self.test_anynumber()
