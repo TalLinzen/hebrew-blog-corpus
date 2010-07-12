0ec28a1762af5b23b5b626ffd484920356e653b7
Tal Linzen <tal.linzen@gmail.com>
2010-07-12 18:25:22 +0300

    More Lucene index improvements

--- lucene_index.py
+++ lucene_index.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# profile.run('execfile("lucene_index.py"); execfile("run.py"); setup_connection(""); test()')
+# profile.run('execfile("lucene_index.py"); execfile("run.py"); setup_connection(""); build_index()')
+# initVM(); from AnalyzerUtils import AnalyzerUtils; AnalyzerUtils.displayTokensWithFullDetails(BlogCorpusAnalyzer(), open('/Users/tal/Dropbox/Hebrew-Blog-Corpus/experiments/t/1').read().replace(' ', '@'))
 import sys, os,  threading, time, codecs, lucene
 from lucene import \
     Document, Field, TermAttribute, TermAttribute, PositionIncrementAttribute, \
@@ -14,6 +15,7 @@ index_dir = '/Users/tal/corpus/lucene_index'
 class IndexCorpus(object):
 
     def __init__(self, index_dir, analyzer):
+        self.metadata = True
         if not os.path.exists(index_dir):
             os.mkdir(index_dir)
 
@@ -43,24 +45,26 @@ class IndexCorpus(object):
                     print "Indexing exception:", e
 
     def index_file(self, path):
+        YES = Field.Store.YES
+        NO = Field.Store.NO
+        NOT_ANALYZED = Field.Index.NOT_ANALYZED
+        ANALYZED = Field.Index.ANALYZED
+        metadata = self.metadata
+
         handle = codecs.open(path, encoding='utf8')
         filename = os.path.basename(path)
-        user_number = WebPage.get(filename).user
-        user_record = list(User.select(User.q.number == user_number))[0]
-        gender = user_record.sex if user_record.sex is not None else "Unknown"
-        birthyear = (str(user_record.birthyear) 
-                if user_record.birthyear is not None else '0')
+        if metadata:
+            user_number = WebPage.get(filename).user
+            user_record = list(User.select(User.q.number == user_number))[0]
+            gender = user_record.sex if user_record.sex is not None else "Unknown"
+            birthyear = (str(user_record.birthyear) 
+                    if user_record.birthyear is not None else '0')
 
         s = handle.read()
 
         pos = s.find(u'\n\n') + 2    # Skip first block: declaration
         nextpos = 0
         sentence_index = 0
-
-        YES = Field.Store.YES
-        NO = Field.Store.NO
-        NOT_ANALYZED = Field.Index.NOT_ANALYZED
-        ANALYZED = Field.Index.ANALYZED
 
         doc = Document()
         sentence_index_field = Field("sentence_index", str(sentence_index), YES,
@@ -71,28 +75,30 @@ class IndexCorpus(object):
         filename_field = Field("filename", '', YES, NOT_ANALYZED)
         contents_field = Field("contents", '', NO, ANALYZED)
         doc.add(sentence_index_field)
-        doc.add(user_field)
-        doc.add(gender_field)
-        doc.add(birthyear_field)
-        doc.add(filename_field)
+        if metadata:
+            doc.add(user_field)
+            doc.add(gender_field)
+            doc.add(birthyear_field)
+            doc.add(filename_field)
         doc.add(contents_field)
 
         while nextpos != len(s):
             nextpos = s.find(u'\n \n', pos)
             if nextpos == -1:
                 nextpos = len(s)
-            sentence_index = sentence_index + 1
             text = s[pos:nextpos]
-            text = text.strip().replace(u' ', u'@')
+            text = text.replace(u' ', u'@')
             pos = nextpos + 2
 
             sentence_index_field.setValue(str(sentence_index))
-            user_field.setValue(user_number)
-            gender_field.setValue(gender)
-            birthyear_field.setValue(birthyear)
-            filename_field.setValue(filename)
+            if metadata:
+                user_field.setValue(user_number)
+                gender_field.setValue(gender)
+                birthyear_field.setValue(birthyear)
+                filename_field.setValue(filename)
             contents_field.setValue(text)
             self.writer.addDocument(doc)
+            sentence_index = sentence_index + 1
 
 class BlogCorpusFilter(PythonTokenFilter):
     too_long_count = 0
@@ -105,32 +111,37 @@ class BlogCorpusFilter(PythonTokenFilter):
         self.posAttr = self.addAttribute(PositionIncrementAttribute.class_)
         self.n_features = 2
         self.feature_index = self.n_features
+        self.token_count = 0
 
     def incrementToken(self):
         if self.feature_index < self.n_features:
             if self.feature_index == 1:
                 self.posAttr.setPositionIncrement(0)
             self.termAttr.setTermBuffer(self.features[self.feature_index])
-            print self.posAttr.positionIncrement, repr(self.termAttr.term())
             self.feature_index += 1
             return True
         else:
-            self.feature_index = 0
+            self.token_count += 1
             if not self.inStream.incrementToken():
                 return False
-
             self.getFeatures(self.termAttr.term())
+            self.termAttr.setTermBuffer(self.features[0])
+            self.feature_index = 1
             return True
 
     declaration = 'word prefix base suffix lemma pos postype gender number construct polarity tense person def pconj pint pprep psub ptemp prb suftype sufgen sufnum sufperson chunk'.split()
 
     def getFeatures(self, arg):
-        if len(arg) == 255:
+        if len(arg) == 0:
+            self.features = ['dummy0000', 'dummy0000']
+        elif len(arg) == 255:
             # Lucene CharTokenizer silly limitation, should find a workaround
             self.__class__.too_long_count += 1
             print 'Too long count: %d' % self.__class__.too_long_count
             self.features = ['dummy1212'] * self.n_features
         else:
+            if arg[0] == '@':
+                arg = arg[1:]
             try:
                 word, prefix, base, suffix, lemma, pos, rest = arg.split('@', 6)
             except ValueError:
@@ -138,7 +149,6 @@ class BlogCorpusFilter(PythonTokenFilter):
                 self.features = ['dummy3434'] * self.n_features
             else:
                 self.features = ['w%s' % word, 'l%s' % lemma]
-                print self.features
 
 
 class BlogCorpusAnalyzer(PythonAnalyzer):
@@ -146,10 +156,11 @@ class BlogCorpusAnalyzer(PythonAnalyzer):
         tokenStream = WhitespaceTokenizer(reader)
         return BlogCorpusFilter(tokenStream)
 
-def search(d=index_dir):
+command1 = u'wלאכול'
+command2 = u'"lשלום lקורא"'
+
+def search(command=command1, d=index_dir):
     initVM()
-    command = u'wלאכול'
-    command = u'"lשלום lקורא"'
     directory = SimpleFSDirectory(File(d))
     searcher = IndexSearcher(directory, True)
     analyzer = StandardAnalyzer(Version.LUCENE_CURRENT)
@@ -159,18 +170,17 @@ def search(d=index_dir):
     print 'Total hits:', res.totalHits
     return [searcher.doc(doc.doc) for doc in res.scoreDocs]
 
-def test():
+def build_index():
     initVM()
     start = datetime.now()
     try:
-        #dir = '/Users/tal/corpus/analyzed/4'
         dir = '/Users/tal/corpus/analyzed'
         #dir = '/Users/tal/Dropbox/Hebrew-Blog-Corpus/experiments/t'
         analyzer = BlogCorpusAnalyzer()
         idx = IndexCorpus(index_dir, analyzer)
         idx.index(dir)
-        #for i in range(4, 10):
-        #    idx.index_dir(dir % i)
+        #for i in range(4, 5):
+        #    idx.index(dir + '/%d' % i)
         idx.finalize()
         end = datetime.now()
         print end - start
